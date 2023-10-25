a5134449d30f50079f286af7f1f9532d51c7cde3
harry.93 <harry.93@kakaopaysec.com>
2023-10-25 20:09:39 +0900

    :construction:

--- Part4-ClassObjectInterface/NestedObject.kt
+++ Part4-ClassObjectInterface/NestedObject.kt
@@ -1,6 +1,8 @@
 package `Part4-ClassObjectInterface`
 
+// 중첩객체를 사용해 Comparator 구현하기.
 data class Person(val name: String) {
+    // 중첩객체인 NameComparator 생성, Comparator<Person> 를 구현했다.
     object NameComparator : Comparator<Person> {
         override fun compare(o1: Person, o2: Person): Int = o1.name.compareTo(o2.name)
     }
